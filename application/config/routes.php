089f031660b5f3929860f8e412ce0fdfc922ee30
Gregorio Setti <gregorio.setti@gmail.com>
2012-10-17 13:11:31 +0200

    Controller and routes for static pages

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,7 +38,8 @@
 |
 */
 
-$route['default_controller'] = "welcome";
+$route['default_controller'] = "static_pages/view";
+$route['(:any)'] = "static_pages/view/$1";
 $route['404_override'] = '';
 
 
