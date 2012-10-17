adfc051a132726f0b5aecd50a313c75c0e0abc1f
Gregorio Setti <gregorio.setti@gmail.com>
2012-10-17 16:00:39 +0200

    Database config for localhost added

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,11 @@
 $active_group = 'default';
 $active_record = TRUE;
 
+// Localhost
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'password';
+$db['default']['database'] = 'musicdepot';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
--- application/controllers/static_pages.php
+++ application/controllers/static_pages.php
@@ -3,11 +3,12 @@
 class Static_pages extends CI_Controller {
 
 	public function view($static_page = "index") {
-		if(!file_exists("application/views/static_pages/".$static_page.".php")) {
+		if (!file_exists("application/views/static_pages/".$static_page.".php")) {
 			show_404();
 		}
 
 		$data["title"] = ucfirst($static_page);
+
 		$this->load->view("static_pages/".$static_page, $data);
 	}
 }
